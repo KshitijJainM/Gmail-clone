2a9ebbc14c2c4ead4afab6ad7b05cff1e9ea9e1a
KshitijJainM <2018ise_kshitijjainm@nie.ac.in>
2021-04-23 00:01:00 +0530

    Add Header and Sidebar

---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+import AppsIcon from "@material-ui/icons/Apps";
+import Avatar from "@material-ui/core/Avatar";
+function Header() {
+	return (
+		<div className="header">
+			<div className="header__left">
+				<IconButton>
+					<MenuIcon />
+				</IconButton>
+				<img
+					src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+					alt=""
+				/>
+			</div>
+			<div className="header__middle">
+				<SearchIcon />
+				<input placeholder="Search mail" type="text" />
+				<ArrowDropDownIcon className="header__inputCaret" />
+			</div>
+
+			<div className="header__right">
+				<IconButton>
+					<HelpOutlineIcon />
+				</IconButton>
+				<IconButton>
+					<SettingsOutlinedIcon />
+				</IconButton>
+				<IconButton>
+					<AppsIcon />
+				</IconButton>
+				<IconButton>
+					<Avatar />
+				</IconButton>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
---
+++ src/Sidebar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Sidebar.css";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import PersonIcon from "@material-ui/icons/Person";
+import InboxIcon from "@material-ui/icons/Inbox";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import SendIcon from "@material-ui/icons/Send";
+import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+import IconButton from "@material-ui/core/IconButton";
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			<Button
+				startIcon={<AddIcon fontSize="large" />}
+				className="sidebar__compose">
+				Compose
+			</Button>
+			<SidebarOption
+				Icon={InboxIcon}
+				title="Inbox"
+				number={54}
+				selected={true}
+			/>
+			<SidebarOption
+				Icon={StarIcon}
+				title="Starred"
+				number={54}
+				// selected={true}
+			/>
+			<SidebarOption Icon={WatchLaterIcon} title="Snoozed" number={5} />
+			<SidebarOption Icon={LabelImportantIcon} title="Important" number={5} />
+			<SidebarOption Icon={SendIcon} title="Sent" number={5} />
+			<SidebarOption Icon={InsertDriveFileIcon} title="Drafts" number={5} />
+			<SidebarOption Icon={ExpandMoreIcon} title="More" />
+			<div className="sidebar__footer">
+				<div className="sidebar__footerIcons">
+					<IconButton>
+						<PersonIcon />
+					</IconButton>
+					<IconButton>
+						<DuoIcon />
+					</IconButton>
+					<IconButton>
+						<PhoneIcon />
+					</IconButton>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Sidebar;
