2960da9f4de338e77d7863a44e3330f1a4add091
KshitijJainM <2018ise_kshitijjainm@nie.ac.in>
2021-04-26 23:04:41 +0530

    Add Mail component

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -4,9 +4,11 @@ import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
+import { useHistory } from "react-router-dom";
 function EmailRow({ id, title, subject, description, time }) {
+	const history = useHistory();
 	return (
-		<div className="emailRow">
+		<div onClick={() => history.push("/mail")} className="emailRow">
 			<div className="emailRow__options">
 				<Checkbox />
 				<IconButton>
--- src/Mail.js
+++ src/Mail.js
@@ -1,7 +1,78 @@
 import React from "react";
 import "./Mail.css";
+import { IconButton } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import EmailIcon from "@material-ui/icons/Email";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { useHistory } from "react-router-dom";
 function Mail() {
-	return <div className="email"></div>;
+	const history = useHistory();
+	return (
+		<div className="mail">
+			<div className="mail__tools">
+				<div className="mailtoolsLeft">
+					<IconButton onClick={() => history.push("/")}>
+						<ArrowBackIcon />
+					</IconButton>
+					<IconButton>
+						<MoveToInboxIcon />
+					</IconButton>
+					<IconButton>
+						<EmailIcon />
+					</IconButton>
+					<IconButton>
+						<ErrorIcon />
+					</IconButton>
+					<IconButton>
+						<DeleteIcon />
+					</IconButton>
+					<IconButton>
+						<WatchLaterIcon />
+					</IconButton>
+					<IconButton>
+						<CheckCircleIcon />
+					</IconButton>
+					<IconButton>
+						<LabelImportantIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVertIcon />
+					</IconButton>
+				</div>
+				<div className="mail__toolsRight">
+					<IconButton>
+						<UnfoldMoreIcon />
+					</IconButton>
+					<IconButton>
+						<PrintIcon />
+					</IconButton>
+					<IconButton>
+						<ExitToAppIcon />
+					</IconButton>
+				</div>
+			</div>
+			<div className="mail__body">
+				<div className="mail__bodyHeader">
+					<h2>Subject</h2>
+					<LabelImportantIcon className="mail__important" />
+					<p>Title</p>
+					<p className="mail__time">10pm</p>
+				</div>
+				<div className="mail__message">
+					<p>this is a message</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Mail;
